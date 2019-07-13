11038f4e57a908324ecec35d0766481e4fe0ac65
AntoineAcquart <antoine.acquart@ynov.com>
2019-07-13 21:47:59 +0200

    add picture

--- angular-electron/src/app/components/files/files.component.ts
+++ angular-electron/src/app/components/files/files.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, NgZone } from "@angular/core";
-import { readdir, stat, mkdir } from "fs";
-import { resolve } from "path";
-import { shell } from "electron";
 import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  FormControl
-} from "@angular/forms";
+  readdir,
+  mkdir,
+  rmdirSync,
+  rmdir,
+  unlinkSync,
+  writeFileSync,
+  writeFile,
+  watch
+} from "fs";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-files",
@@ -15,15 +17,16 @@ import {
   styleUrls: ["./files.component.scss"]
 })
 export class FilesComponent implements OnInit {
+  private folderExist: boolean;
   private folderForm: FormGroup;
-  private path: string = process.cwd() + "/user-data";
-  private entries: Array<string> = [];
-
+  private path: string = process.cwd() + "/src/assets/user-data";
+  private folders: [string, [string]][] = [];
   constructor(private fb: FormBuilder, private _ngZone: NgZone) {
     this.getUserData();
   }
 
   ngOnInit() {
+    this.folderExist = false;
     this.folderForm = this.fb.group({
       folderName: [
         "",
@@ -38,24 +41,36 @@ export class FilesComponent implements OnInit {
   }
 
   private checkFolderExist(newFolder: string) {
-    let result = true;
-    const targetPath = resolve(this.path, newFolder);
-    stat(targetPath, (err, stats) => {
-      if (err) {
-        console.error(err);
+    let exist = false;
+    for (let i = 0; i < this.folders.length; i++) {
+      if (newFolder == this.folders[i][0]) {
+        exist = true;
+        break;
       }
-      result = stats.isDirectory() ? true : false;
-    });
-    return result;
+    }
+    this.folderExist = exist;
   }
 
-  private async getUserData() {
-    readdir(this.path, (err: Error, files: [string]) => {
+  private getUserData() {
+    watch(this.path);
+    readdir(this.path, (err: Error, folders: [string]) => {
       if (err) {
         console.error(err);
       }
       this._ngZone.run(() => {
-        this.entries = files;
+        for (let i = 0; i < folders.length; i++) {
+          readdir(
+            this.path + "/" + folders[i],
+            (err: Error, files: [string]) => {
+              if (err) {
+                console.error(err);
+              }
+              this._ngZone.run(() => {
+                this.folders[i] = [folders[i], files];
+              });
+            }
+          );
+        }
       });
     });
   }
@@ -65,10 +80,8 @@ export class FilesComponent implements OnInit {
   }
 
   private createFolder() {
-    if (
-      this.folderForm.valid &&
-      !this.checkFolderExist(this.folderForm.value["folderName"])
-    ) {
+    this.checkFolderExist(this.folderForm.value["folderName"]);
+    if (this.folderForm.valid && !this.folderExist) {
       mkdir(
         this.path + "/" + this.folderForm.value["folderName"],
         (err: Error) => {
@@ -79,8 +92,48 @@ export class FilesComponent implements OnInit {
           }
         }
       );
-    } else {
-      console.log("le dossier existe déjà");
     }
+  }
+
+  private deleteFolder(folder: string) {
+    let folderIndex = this.folders.findIndex(value => value[0] == folder);
+    for (let i = 0; i < this.folders[folderIndex][1].length; i++) {
+      unlinkSync(
+        this.path + "/" + folder + "/" + this.folders[folderIndex][1][i]
+      );
+    }
+
+    rmdirSync(this.path + "/" + folder);
+    this.folders.splice(folderIndex, 1);
+  }
+
+  private addPicture(event, folder: string) {
+    let file = event.target.files[0];
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    this._ngZone.run(() => {
+      reader.onload = function() {
+        console.log(reader.result);
+        let img = String(reader.result);
+        let data = img.split(",");
+        let buffer = Buffer.from(data[1], "base64");
+        console.log(buffer);
+
+        writeFileSync(
+          process.cwd() +
+            "/src/assets/user-data" +
+            "/" +
+            folder +
+            "/" +
+            file.name,
+          buffer
+        );
+      };
+      this.getUserData();
+    });
+    reader.onerror = function(error) {
+      console.log("Error: ", error);
+    };
   }
 }
